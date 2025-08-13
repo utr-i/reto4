278f1745e06d7d06ba5ff75aafe3f23c32cc22ca
utr-i <f.elisondo@itecriocuarto.org.ar>
2025-08-13 19:20:26 -0300

    complete

---
+++ tailwind/js/script.js
@@ -0,0 +1,5 @@
+    const toggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+    toggle.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
